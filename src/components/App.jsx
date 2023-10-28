daa539d3332071a244e30a6f53259bbe9ab4d5d0
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-10-28 22:47:18 +0200

    Turn App into class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,29 @@
+import React, { Component } from 'react';
 import { Searchbar } from './searchbar/Searchbar';
 import { ImageGallery } from './imageGallery/ImageGallery';
 import { Button } from './button/Button';
 import { Loader } from './loader/Loader';
 import { Modal } from './modal/Modal';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Searchbar />
-      <ImageGallery />
-      <Button />
-      <Loader />
-      <Modal />
-    </div>
-  );
-};
+export class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Searchbar />
+        <ImageGallery />
+        <Button />
+        <Loader />
+        <Modal />
+      </div>
+    );
+  }
+}
