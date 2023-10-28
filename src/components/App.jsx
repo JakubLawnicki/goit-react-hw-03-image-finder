0a1019e494b756d193a89ddd2edc742ad0cc59b1
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-10-28 23:44:37 +0200

    Add fetching data after form submit
    I related status with input search value and added functionality of fetching data from API after form submit.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,33 @@ import { ImageGallery } from './imageGallery/ImageGallery';
 import { Button } from './button/Button';
 import { Loader } from './loader/Loader';
 import { Modal } from './modal/Modal';
+import axios from 'axios';
 
 export class App extends Component {
+  state = {
+    search: '',
+  };
+
+  key = '39408745-32e39ba950214e66e33847e97';
+
+  searchChange = value => {
+    this.setState(() => {
+      return {
+        search: value,
+      };
+    });
+  };
+
+  searchSubmit = async () => {
+    const response = await axios.get(
+      `https://pixabay.com/api/?q=${this.state.search}&page=1&key=${this.key}&image_type=photo&orientation=horizontal&per_page=12`
+    );
+    console.log(response.data);
+  };
+
   render() {
+    const { search } = this.state;
+    console.log(this.state);
     return (
       <div
         style={{
@@ -18,7 +42,11 @@ export class App extends Component {
           color: '#010101',
         }}
       >
-        <Searchbar />
+        <Searchbar
+          onSubmit={this.searchSubmit}
+          change={this.searchChange}
+          inputValue={search}
+        />
         <ImageGallery />
         <Button />
         <Loader />
--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,18 +1,28 @@
 // import PropTypes from 'prop-types';
 import styles from './searchbar.module.css';
 
-export function Searchbar({ onSubmit }) {
+export function Searchbar({ onSubmit, inputValue, change }) {
   return (
     <header className={styles.searchbar}>
-      <form className={styles.form}>
+      <form
+        className={styles.form}
+        onSubmit={e => {
+          e.preventDefault();
+          onSubmit();
+        }}
+      >
         <button type="submit" className={styles.button}>
           <span className={styles['button-label']}>Search</span>
         </button>
 
         <input
           className={styles.input}
+          onChange={e => {
+            change(e.target.value);
+          }}
           type="text"
           autocomplete="off"
+          value={inputValue}
           autofocus
           placeholder="Search images and photos"
         />
